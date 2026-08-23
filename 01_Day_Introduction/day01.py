608876034a18f533099642a647218562da46c110
notvolochkova <68059556+notvolochkova@users.noreply.github.com>
2026-08-23 20:21:17 +0400

    day01 done

---
+++ 01_Day_Introduction/day01.py
@@ -0,0 +1,24 @@
+print("Hello World, Enter Python")
+
+#basic math operations
+print(2 + 3)
+print(3 - 1)    
+print(2*3)      #multiplication
+print(3+2)      #addition
+print(3-6)      #subtraction
+print(3 / 2)    #division
+print(3 % 2)    #modulus
+print(3 // 2)   #floor division
+
+#space insensitive
+print(type(10))                 #int
+print(type(3.14))               #float
+print(type(1 + 3j))             #complex
+print(type('Asabeneh'))         #string
+print(type([1,2 ,3]))           #list
+print(type({'name': 'Asabeneh', #dictionary
+             'age':'24'}))
+print(type({9.8, 3.14, 2.7}))     #tuple
+
+float_tuple = {9.8, 3.14, 2.7}
+print(float_tuple)
